03682fa60743d7fc023debd953449518e39dfdd0
Tautvydas Belgeras <tautvydas.b@8devices.com>
2025-10-25 19:11:02 +0300

    Add terraform with EC2 provisioning in AWS

---
+++ main.tf
@@ -0,0 +1,57 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-north-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_security_group" "ec2_sg" {
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_key_pair" "ec2_key" {
+    key_name = "ec2-key"
+    # TODO this won't work in CI/CD
+    public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_instance" "ubuntu_ec2" {
+  ami                    = data.aws_ami.ubuntu.id
+  key_name               = aws_key_pair.ec2_key.key_name
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+}
